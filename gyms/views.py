595c593b8cfc03863738f00c07126249ba3fd54a
bankaiogx <ayazarbaz71@gmail.com>
2026-04-15 11:19:37 +0100

    updated gym/views

--- gyms/views.py
+++ gyms/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Gym
 
 
 def home(request):
     return render(request, 'gyms/home.html')
+
+
+def gym_list(request):
+    gyms = Gym.objects.all()
+    return render(request, 'gyms/gym_list.html', {'gyms': gyms})
