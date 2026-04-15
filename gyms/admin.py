860ff872b3384d979c2c274a989592ba4c150ef8
bankaiogx <ayazarbaz71@gmail.com>
2026-04-15 10:15:58 +0100

    edited views.py

--- gyms/admin.py
+++ gyms/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Amenity, Gym, Review, Favourite, GymAmenity
 
-# Register your models here.
+
+admin.site.register(Amenity)
+admin.site.register(Gym)
+admin.site.register(Review)
+admin.site.register(Favourite)
+admin.site.register(GymAmenity)
