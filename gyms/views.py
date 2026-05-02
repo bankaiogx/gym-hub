ed901d4a90f6ab615d4a3dae24d8c798726b9a41
bankaiogx <ayazarbaz71@gmail.com>
2026-05-02 13:55:20 +0100

    added images. also had bug where video was static and images werent coming through. fixed

--- gyms/views.py
+++ gyms/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Avg
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import GymForm, ReviewForm
@@ -10,21 +11,31 @@ def home(request):
 
 
 def gym_list(request):
-    gyms = Gym.objects.all()
+    gyms = Gym.objects.annotate(average_rating=Avg('reviews__rating'))
     return render(request, 'gyms/gym_list.html', {'gyms': gyms})
 
 
 def gym_detail(request, slug):
     gym = get_object_or_404(Gym, slug=slug)
     reviews = gym.reviews.select_related('user')
-    review_form = ReviewForm() if request.user.is_authenticated else None
+    average_rating = gym.reviews.aggregate(Avg('rating'))['rating__avg']
+    user_review = None
+    review_form = None
+
+    if request.user.is_authenticated:
+        user_review = reviews.filter(user=request.user).first()
+        if user_review is None:
+            review_form = ReviewForm()
+
     return render(
         request,
         'gyms/gym_detail.html',
         {
             'gym': gym,
             'reviews': reviews,
+            'average_rating': average_rating,
             'review_form': review_form,
+            'user_review': user_review,
         },
     )
 
@@ -51,6 +62,9 @@ def add_review(request, slug):
     if request.method != 'POST':
         return redirect('gym_detail', slug=gym.slug)
 
+    if gym.reviews.filter(user=request.user).exists():
+        return redirect('gym_detail', slug=gym.slug)
+
     form = ReviewForm(request.POST)
     if form.is_valid():
         review = form.save(commit=False)
@@ -60,12 +74,15 @@ def add_review(request, slug):
         return redirect('gym_detail', slug=gym.slug)
 
     reviews = gym.reviews.select_related('user')
+    average_rating = gym.reviews.aggregate(Avg('rating'))['rating__avg']
     return render(
         request,
         'gyms/gym_detail.html',
         {
             'gym': gym,
             'reviews': reviews,
+            'average_rating': average_rating,
             'review_form': form,
+            'user_review': None,
         },
     )
